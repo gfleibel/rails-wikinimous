fdd5c54c57c72e18c17fea6afcdcc390dffc8f48
Gabriel Leibel <gfleibel@hotmail.com>
2023-05-23 14:50:26 -0300

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Create 10 fake articles
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(number: rand(1..3)).join("\n\n"),
+  )
+end
